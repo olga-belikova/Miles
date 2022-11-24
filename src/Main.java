f5e83c9375b733ce38fe2c8e3d89537cd19fbce1
Olga Belikova <belko.belikova@gmail.com>
2022-11-24 22:22:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 500;
+        int oneMileCost = 20;
+        int miles = amount / oneMileCost;
+
+        System.out.println("Начислено миль за покупку: " + miles);
+
+    }
+}
